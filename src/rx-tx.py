f540a49a9b7b11f0d8b8c4c8b5564c09403bdbd6
Viktor Myronovych <viktor.myronovych@gmail.com>
2024-01-20 10:27:37 +0200

    Add simple nodejs tcp server which will be listening to messages from ch9121 and print message to console

--- src/rx-tx.py
+++ src/rx-tx.py
@@ -9,15 +9,10 @@ RST = Pin(17, Pin.OUT,Pin.PULL_UP)
 CFG.value(1)
 RST.value(1)
 
-#print("writing to uart 1")
-#uart0.write("hellow for uart 1")
-
 while True:
     uart0.write("hello from pico")
-    while uart0.any() > 0:    #Channel 0 is spontaneous and self-collecting
+    while uart0.any() > 0:
         rxData0 = uart0.read()
         uart0.write(rxData0)
         print(rxData0)
-        time.sleep(10)
-    time.sleep(10) 
-
+    time.sleep(10)
